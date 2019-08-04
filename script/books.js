f7269184e73b27df42e8f0a8163a99b203790712
Irina Winter <lobanova.irina.96@mail.ru>
2019-08-04 21:27:27 +0300

    /

---
+++ script/books.js
@@ -0,0 +1,14 @@
+var books = [
+	{
+		img: "book",
+		name: "Язык творчества",
+		author: "Феодор Леонид",
+		prize: 500,
+		specialty: "42.02.02 Дизайн (по отраслям)",
+		year: 2019,
+		format: "60x90/16",
+		pages: 120,
+		annotation: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		reissue: 2
+	}
+]
---
+++ script/renderBook.js
@@ -0,0 +1,10 @@
+function renderBook(elment) {
+	var div = document.createElement('div');
+	div.className = "book";
+
+	var img = document.createElement('img');
+	img.src = "img/" + element.img + "jpg";
+	console.log(books);
+}
+
+renderBook();
