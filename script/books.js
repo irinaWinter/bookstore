9481beb5b7c22723f86e1f68315f2dff06cca573
irinaWinter <lobanova.irina.96@mail.ru>
2019-08-09 16:22:22 +0300

    Заполнение базы данных

--- script/books.js
+++ script/books.js
@@ -12,3 +12,19 @@ var books = [
 		reissue: 2
 	}
 ]
+
+function getSums(arr) {
+	var result = [];
+	if (!arr.length) return result;
+
+	var totalSum = arr.reduce(function(sum, item) {
+		result.push(sum);
+		return sum + item;
+	});
+	result.push(totalSum);
+
+	return result;
+}
+
+alert(getSums([1, 2, 3, 4, 5]));
+alert( getSums([-2, -1, 0, 1]) );
