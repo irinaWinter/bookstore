28c6c08a69d37f86c726beeb0b08fda83f3a7175
irinaWinter <lobanova.irina.96@mail.ru>
2019-09-13 16:54:38 +0300

    Фильтр ВП

--- script/filter.js
+++ script/filter.js
@@ -1,41 +1,38 @@
 var profession__item = document.querySelectorAll('.profession__item');
-var profession__item_selected = document.querySelectorAll('.profession__item_selected');
 var myBooks = document.querySelectorAll('.book');
+var profession__item_selected;
 
 
 for (var j = 0; j < profession__item.length; j++) {
 
 	profession__item[j].addEventListener('click', function () {
+		var _this = this;
+		if (profession__item_selected == undefined) {
+			this.classList.toggle('profession__item_selected');
+			profession__item_selected = document.querySelectorAll('.profession__item_selected');
 		
-		
-		
-
-		_this = this;
-		this.classList.toggle('profession__item_selected');
-
-
-
-		// if (this.classList.contains("profession__item_selected")) {
-		// 	books.forEach(function(element, index) {
-		// 		if (element.specialty == _this.getAttribute("Name")) {
-
-		// 		} else {
-		// 			myBooks[index].classList.add("hide");	
-		// 		}	
-		// 	});
-		// } else {
-		// 	books.forEach(function(element, index) {
-		// 		if (element.specialty == _this.getAttribute("Name")) {
-
-		// 		} else {
-		// 			myBooks[index].classList.remove("hide");	
-		// 		}	
-
-
+			books.forEach(function(element, index) {
+				if (element.specialty !== _this.getAttribute("Name")) {
+					myBooks[index].classList.add("hide");	
+				} 
 			});
-		// }
+		} else {
+			profession__item.forEach(function(e) {
+				e.classList.remove("profession__item_selected");
+			});
 
-console.log(profession__item_selected.length);
+
+				// Переписать
+
+			this.classList.toggle('profession__item_selected');		
+			books.forEach(function(element, index) {
+				if (element.specialty !== _this.getAttribute("Name")) {
+					myBooks[index].classList.add("hide");	
+				} 
+			});
+			
+		}	
+	});
 };
 
 
