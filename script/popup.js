f2895b7939b9edf66d8c8dd218dd05846780f1c9
irinaWinter <lobanova.irina.96@mail.ru>
2019-09-30 17:41:15 +0300

    /

--- script/popup.js
+++ script/popup.js
@@ -15,6 +15,7 @@ var detail__format = document.createElement('p');
 var detail__pages = document.createElement('p');
 var detail__annotation = document.createElement('p');
 var detail__reissue = document.createElement('p');
+
 var createPopup = {
 	create: function(element) {
 		var main = document.querySelector('main');
@@ -23,17 +24,14 @@ var createPopup = {
 		body.style.overflow = "hidden";
 
 		// плашка
-		// var popUp = document.createElement('div');
 			popup.className = "popup";
 			main.append(popup);
 
 		// окно для контента
-		
 			detail.className = "detail";
 			popup.append(detail);
 
 		// крестик для закрытия окна
-
 			detail__close.className = "detail__close";
 			detail__close.src = "img/close.png";
 			detail.append(detail__close);	
@@ -47,69 +45,52 @@ var createPopup = {
 			detail__illustration.append(detail__img);
 
 
-		
-
-		
-
-
-
-
-
 		// БЛОК С ТЕКСТОВОЙ ИНФОРМАЦИЕЙ
 
 			detail__text.className = "detail__text";
 			detail.append(detail__text);
 
 		// цена
-
 			detail__price.className = "detail__price";
 			detail__price.textContent = element.price + " ₽";
 			detail__text.append(detail__price);
 
 		// название издания
-
 			detail__name.className = "detail__name";
 			detail__name.textContent = element.name;
 			detail__text.append(detail__name);
 
 		// автор
-	
 			detail__author.className = "detail__author";
 			detail__author.textContent = element.author;
 			detail__text.append(detail__author);
 
 		// специальность
-
 			detail__specialty.className = "detail__info";
 			detail__specialty.textContent = "Специальность: " + element.specialty;
 			detail__text.append(detail__specialty);
 
 		// год издания
-		
 			detail__year.className = "detail__info";
 			detail__year.textContent = "Год издания: " + element.year;
 			detail__text.append(detail__year);
 
 		// формат
-		
 			detail__format.className = "detail__info";
 			detail__format.textContent = "Формат: " + element.format;
 			detail__text.append(detail__format);
 
 		// объем
-		
 			detail__pages.className = "detail__info";
 			detail__pages.textContent = "Объём: " + element.pages + " с.";
 			detail__text.append(detail__pages);
 
 		// аннотация
-		
 			detail__annotation.className = "detail__annotation";
 			detail__annotation.textContent = element.annotation;
 			detail__text.append(detail__annotation);
 
 		// переиздание
-		
 			detail__reissue.className = "detail__info";
 			detail__reissue.textContent = "Количество переизданий: " + element.reissue;
 			detail__text.append(detail__reissue);
@@ -118,10 +99,7 @@ var createPopup = {
 		popup.remove();
 		body.style.overflow = "visible";
 	}
-
-
 }
-
 
 myBooks.forEach(function(e, i) {	
 	e.addEventListener("click", function() {
@@ -132,3 +110,11 @@ myBooks.forEach(function(e, i) {
 detail__close.addEventListener("click", function() {
 	createPopup.close();
 });
+
+popup.addEventListener("click", function(event) {
+	if (event.target == this) {
+		createPopup.close();
+	}
+})
+
+
